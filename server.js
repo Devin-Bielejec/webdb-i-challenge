285e98736f592431a73067e695397ac608e09032
devin-bielejec <devin.bieleje@gmail.com>
2019-09-19 12:57:33 -0400

    Get and create requests complete

--- server.js
+++ server.js
@@ -6,4 +6,27 @@ const server = express();
 
 server.use(express.json());
 
-module.exports = server;
+//get all accounts
+server.get("/api/accounts", (req, res) => {
+    db("accounts")
+    .then(accounts => res.json(accounts))
+    .catch(error => res.status(500).json({message: "Failed to get accounts"}))
+})
+
+//create an account
+server.post("/api/accounts", (req, res) => {
+    const { name, budget } = req.body;
+
+    !name ? res.status(404).json({message: "Name is required"}) : null;
+
+    !budget ? res.status(404).json({message: "Budget is required"}) : null;
+
+    db("accounts").insert({ name, budget })
+    .then(accounts => res.json(accounts) )
+    .catch(error => res.status(500).json({message: "Server Error inserting this account"}))    
+
+})
+
+const port = 5000;
+
+server.listen(port, () => console.log(`Listening on port ${port}`));module.exports = server;
